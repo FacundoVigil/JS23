8aa00463f55aa4d77c1f63abd3dc93afab819788
Facundo Vigil <facuvigil.23@gmail.com>
2024-09-17 19:38:23 -0300

    cuarto

--- js/main.js
+++ js/main.js
@@ -23,3 +23,23 @@ Persona.filtrarPorEdad= function(edad)
 {let personasFiltradas= personas.filter(persona=>persona.edad>=edad);
 console.log("Personas filtradas:", personasFiltradas);
 return personasFiltradas;};
+
+let nombre = prompt("Ingrese el nombre de la persona:");
+
+let edad = parseInt(prompt("Ingrese la edad de la persona:"));
+Persona.agregar(nombre, edad);
+
+let opcion = prompt("¿Qué desea hacer? (1) Obtener todas las personas, (2) Obtener persona por nombre, (3) Filtrar por edad:");
+switch (opcion) {
+case "1":Persona.obtenerTodas();
+break;
+case "2": let nombreBuscar = prompt("Ingrese el nombre de la persona a buscar:");
+Persona.obtenerPorNombre(nombreBuscar);
+break;
+case "3": let edadFiltrar = parseInt(prompt("Ingrese la edad para filtrar personas mayores o iguales a esa edad:"));
+Persona.filtrarPorEdad(edadFiltrar);
+break;
+
+default:
+console.log("Opción no válida.");
+}
